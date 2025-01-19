5787365400d93207d634224c6c7e5c3db8436640
abdalla-shaker <abdallashaker143@gmail.com>
2025-01-19 18:13:30 +0200

    second commit

--- play.js
+++ play.js
@@ -15,12 +15,76 @@
 
 // ***************** OBJECTS ***************** //
 
-const person = {
-  name: "Abdullah",
-  age: 22,
-  greet() {
-    console.log(`Hi, I am ${this.name}, and I am ${this.age} years old`);
-  },
+// const person = {
+//   name: "Abdullah",
+//   age: 22,
+//   greet() {
+//     console.log(`Hi, I am ${this.name}, and I am ${this.age} years old`);
+//   },
+// };
+
+// const printPersonName = ({name}) => {
+//   console.log(name);
+// }
+
+// printPersonName(person)
+
+// const {name, age} = person;
+
+// console.log(name, age)
+
+// person.greet();
+
+// const hobbies = ['sports', 'cooking']
+
+// const [hobby1, hobby2] = hobbies;
+
+// console.log(hobby1, hobby2);
+
+// for (let hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+// console.log(hobbies.map(hobby => `Hobby: ${hobby}`))
+// console.log(hobbies);
+
+// hobbies.push("programming");
+// console.log(hobbies);
+
+// To copy an array:
+
+// const copiedHobbies = [...hobbies] // *OR: const copiedHobbies = hobbies.slice();
+// *IT Also works with objects.
+
+// console.log(copiedHobbies);
+
+// The spread operators:
+
+// const arrMaker = (...args) => args;
+
+// console.log(arrMaker(1, 2, 3, 4, 5))
+
+// ********************** ASYNC AND PROMISES **********************
+const fetchName = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Abdullah");
+    }, 1500);
+  });
+  return promise;
 };
 
-person.greet();
+setTimeout(() => {
+  console.log("getting user name please wait...");
+  fetchName()
+    .then((fetchedName) => {
+      console.log(`Welcome ${fetchedName}.`);
+      return fetchName();
+    })
+    .then((fetchedName) => {
+      console.log(`Welcome ${fetchedName} again.`);
+    });
+}, 2000);
+
+// console.log("hello");
+// console.log("hi");
